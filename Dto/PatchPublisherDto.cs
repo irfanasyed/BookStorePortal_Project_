c7f576c11b12ffdafe80923256f6e8012f992f90
Ruchika Deore <ruchikadeore163@gmail.com>
2024-11-19 10:57:46 +0530

    PublisherDto And PubInfoDto Committed

---
+++ Dto/PatchPublisherDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStorePortal_Project_.Dto
+{
+    public class PatchPublisherDto
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Publisher Name should be in the range of 5-30")]
+        public string? PubName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "City Name should be in the range of 5-30")]
+
+        public string? City { get; set; }
+        [Required]
+        public string? State { get; set; }
+        [Required]
+        public string? Country { get; set; }
+    }
+}
---
+++ Dto/UpdatePubInfoDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStorePortal_Project_.Dto
+{
+    public class UpdatePubInfoDto
+    {
+        [Required]
+        public string? Logo { get; set; }
+
+        [Required]
+        public string? PrInfo { get; set; }
+    }
+}
