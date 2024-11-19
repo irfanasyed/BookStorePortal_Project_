4f934ac95a78dd5f2f11ff46bb5d2e5bb6285b78
Nishtha <nishthaoswal@gmail.com>
2024-11-19 13:53:15 +0530

    Mapping commited

--- BookMapping/Mapping.cs
+++ BookMapping/Mapping.cs
@@ -1,6 +1,18 @@
-﻿namespace BookStorePortal_Project_.BookMapping
+﻿using AutoMapper;
+using BookStorePortal_Project_.Dto;
+using BookStorePortal_Project_.Models;
+
+namespace BookStorePortal_Project_.BookMapping
 {
-    public class Mapping
+    public class Mapping : Profile
     {
+        public Mapping() 
+        {
+            CreateMap<Store,CreateStoreDto>().ReverseMap();
+            CreateMap<Store, ResponseStoreDto>().ReverseMap();
+            CreateMap<Store, UpdateStoreDto>().ReverseMap();
+            CreateMap<Store, PatchStoreDto>().ReverseMap();
+
+        }
     }
 }
